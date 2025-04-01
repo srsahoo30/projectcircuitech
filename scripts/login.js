f21ca1398f5ae5450b0da097dfc18643554888d7
apsahoo24 <apsahoo24@gmail.com>
2025-04-01 23:33:43 +0530

    PathFix 2.0

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,24 @@
 document.getElementById('login-form').addEventListener('submit', function (e) {
     e.preventDefault();
-  
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-  
+
     fetch('data/credentials.json')
-      .then(response => response.json())
-      .then(data => {
-        const user = data.users.find(user => user.username === username && user.password === password);
-        if (user) {
-            window.location.href = window.location.pathname.replace('index.html', '') + 'dashboard';
-        } else {
-          document.getElementById('error-message').textContent = 'Invalid username or password';
-        }
-      })
-      .catch(error => console.error('Error fetching credentials:', error));
-  });
+        .then(response => response.json())
+        .then(data => {
+            const user = data.users.find(user => user.username === username && user.password === password);
+            if (user) {
+                const baseUrl = window.location.origin;
+
+                if (baseUrl.includes("srsahoo30.github.io")) {
+                    window.location.href = "/projectcircuitech/dashboard";
+                } else {
+                    window.location.href = "/dashboard";
+                }
+            } else {
+                document.getElementById('error-message').textContent = 'Invalid username or password';
+            }
+        })
+        .catch(error => console.error('Error fetching credentials:', error));
+});
